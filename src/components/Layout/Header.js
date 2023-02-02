3e4e9d993332d0bdbba2f1f11eef5410e48c6dfd
= <rukshanlancer@gmail.com>
2023-02-02 17:24:45 +0800

    header design

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,21 @@
 import { Fragment } from 'react';
 import classes from './Header.module.css';
 import image from '../../assets/pizza.jpg';
+import HeaderButton from './HeaderCartButton';
 
 function Header(props) {
     return (
         <Fragment>
-            <header className={classes.header}>
-                <h1>Food Order</h1>
-                <button>Cart</button>
-            </header>
+            <div className={classes.nav}>
+                <header className={classes.header}>
+                    <h1>Food Order</h1>
+                    <HeaderButton />
+                </header>
+            </div>
             <div className={classes['main-image']}>
                 <img src={image} alt='Pizza' />
             </div>
+
         </Fragment>
     );
 }
